301cda79070955ae5e82403539f5554a26f1b38e
diystring <diystring@qq.com>
2018-11-29 23:20:42 +0800

    添加自定义代码片段

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   activated() {
+   }
+}
+</script>
+
+<style>
+/* 模块样式 */
+body{
+background-color: #eee;
+} 
+
+</style>
